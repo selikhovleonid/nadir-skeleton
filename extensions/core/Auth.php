184c14cf2e955db6c8116c51df8413faa81b9c53
coon <selikhovleonid@ya.ru>
2017-11-15 17:58:43 +0300

    PhpDoc was added to the Auth class.

--- extensions/core/Auth.php
+++ extensions/core/Auth.php
@@ -1,31 +1,48 @@
 <?php
-/**
- * This is the auth general class for custom extension.
- * @author Leonid Selikhov
- */
 
 namespace extensions\core;
 
 use nadir\core\Request;
 use nadir\core\AppHelper;
 
+/**
+ * This is the auth general class for custom extension.
+ * @author Leonid Selikhov
+ */
 class Auth extends AbstractAuth
 {
-    protected $request     = null;
+    /** @var \nadir\core\Request The request object. */
+    protected $request = null;
+
+    /** @var array The route config. */
     protected $routeConfig = null;
+
+    /** @var mixed The occured error instance. */
     protected $error       = null;
 
+    /**
+     * The constructor receives the request object to read the route configuration.
+     * @param \nadir\core\Request $oRequest The request object.
+     */
     public function __construct(Request $oRequest)
     {
         $this->request     = $oRequest;
         $this->routeConfig = AppHelper::getInstance()->getRouteConfig();
     }
 
+    /**
+     * It checks cookies according custom algorithm.
+     * @param array $aCookies The associated array of cookies.
+     */
     protected function checkCookies(array $aCookies)
     {
         // Put your code here...
     }
 
+    /**
+     * The main executable method of this class.
+     * @throws \Exception It's throwed if 'auth' option wasn't defined.
+     */
     public function run()
     {
         if (!isset($this->routeConfig['auth'])) {
@@ -35,11 +52,17 @@ class Auth extends AbstractAuth
         $this->checkCookies(!is_null($mCookies) ? $mCookies : array());
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function isValid()
     {
         return is_null($this->error);
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function onFail()
     {
         // Put your code here...
